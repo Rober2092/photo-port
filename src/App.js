b70c8e5b444f734519280f3cf909c51c0e036d7a
Rober2092 <90662615+Rober2092@users.noreply.github.com>
2022-07-13 01:31:46 -0700

    update code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import About from './components/About';
+
+function App() {
+
+  return (
+    <div>
+      <main>
+        <About> who I am </About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
