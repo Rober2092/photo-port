dc25aa3f2752ab5800cd6a521ddb5600ed9cbc01
Rober2092 <90662615+Rober2092@users.noreply.github.com>
2022-07-17 21:14:02 -0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import nav from './components/nav';
 import About from './components/About';
-
 function App() {
 
   return (
     <div>
+      <nav></nav>
       <main>
         <About> who I am </About>
       </main>
